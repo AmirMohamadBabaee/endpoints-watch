949f7acda800a6d42c9270d213074694885fab16
Amir01 <iran.volleyball79@gmail.com>
2023-01-24 01:20:30 +0330

    add: add Endpoint and Request admin class

--- endpoint/admin.py
+++ endpoint/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Endpoint, Request
 # Register your models here.
+
+@admin.register(Endpoint)
+class EndpointAdmin(admin.ModelAdmin):
+
+    list_display = ['endpoint', 'threshold', 'fail_times']
+    list_editable = ['threshold',]
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+
+    list_display = ['get_endpoint', 'created_at', 'result']
+
